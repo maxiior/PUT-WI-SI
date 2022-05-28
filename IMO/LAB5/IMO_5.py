ec6a3e2713ead189e2ad9e71a95c3941e7a082c7
maxiior <59049370+maxiior@users.noreply.github.com>
2022-05-28 21:13:55 +0200

    5

--- IMO/LAB5/IMO_5.py
+++ IMO/LAB5/IMO_5.py
@@ -59,7 +59,7 @@ class LocalSearch:
                     c.insert(0, best_vertex)
                 else:
                     sorted_array = np.sort(scores_matrix)[:, :2]
-                    if sorted_array != []:
+                    if sorted_array.size != 0:
                         regret = np.array([i[1]-i[0] for i in sorted_array])
                         idx = np.argmax(regret - np.min(scores_matrix, axis=1))
                         best_vertex = vertexes[idx]
@@ -75,13 +75,13 @@ class LocalSearch:
         return self.distance_matrix
 
     def get_scores(self, matrix):
-        matrix = deepcopy(matrix)
-        matrix[0] += [matrix[0][0]]
-        matrix[1] += [matrix[1][0]]
-        s1 = sum(self.distance_matrix[matrix[0][i], matrix[0][i+1]]
-                 for i in range(len(matrix[0]) - 1))
-        s2 = sum(self.distance_matrix[matrix[1][i], matrix[1][i+1]]
-                 for i in range(len(matrix[1]) - 1))
+        m = deepcopy(matrix)
+        a = np.append(m[0], m[0][0])
+        b = np.append(m[1], m[1][0])
+        s1 = sum(self.distance_matrix[a[i], a[i+1]]
+                 for i in range(len(a) - 1))
+        s2 = sum(self.distance_matrix[b[i], b[i+1]]
+                 for i in range(len(b) - 1))
         return s1 + s2
 
     def get_random_cycles(self):
@@ -189,200 +189,104 @@ class EvolutionSearch:
     def __init__(self, local_search):
         self.ls = local_search
 
-    def combine(self, sol1, sol2):
-        sol1, sol2 = deepcopy(sol1), deepcopy(sol2)
-        
-        remaining = []
-        for cyc1 in sol1:
-            n = len(cyc1)
-            if n == 1:
-                continue
-            for i in range(n):
-                p, q = cyc1[i], cyc1[(i+1)%n]
-                if p == -1 or q == -1 or p == q:
-                    continue
-                found = False
-                for cyc2 in sol2:
-                    m = len(cyc2)
-                    for j in range(m):
-                        u, v = cyc2[j], cyc2[(j+1)%m]
-                        if (p == u and q == v) or (p == v and q == u):
-                            found = True
-                            break
-                    if found:
-                        break
-                        
-                if not found:
-                    remaining.append(cyc1[i])
-                    remaining.append(cyc1[(i+1)%n])
-                    cyc1[i] = -1
-                    cyc1[(i+1)%n] = -1
-                    
-            for i in range(1, n):
-                x, y, z = cyc1[(i-1)%n], cyc1[i], cyc1[(i+1)%n]
-                if x == z == -1 and y != -1:
-                    remaining.append(y)
-                    cyc1[i] = -1
-                    
-            for i in range(1, n):
-                x = cyc1[i]
-                if x != -1 and np.random.rand() < 0.2:
-                    remaining.append(x)
-                    cyc1[i] = -1
-                    
-        a = [x for x in sol1[0] if x != -1]
-        b = [x for x in sol1[1] if x != -1]
-        return self.ls.regret_heuristics([a, b], remaining)
+    def combine_parents(self, cycles1, cycles2, threshold=0.2):
+        cycles1, cycles2 = deepcopy(cycles1), deepcopy(cycles2)
 
-    def search(self, population_size=3, limit=10, score_threshold=40, iterations_threshold=300, local=True):
+        memory = []
+        for c1 in cycles1:
+            n = len(c1)
+            for i in range(n):
+                a, b = c1[i], c1[(i+1) % n]
+                if a == -1 or b == -1 or a == b:
+                    continue
+                check = False
+                for c2 in cycles2:
+                    m = len(c2)
+                    for j in range(m):
+                        c, d = c2[j], c2[(j+1) % m]
+                        if (a == c and b == d) or (a == d and b == c):
+                            check = True
+                            break
+                    if check:
+                        break
+
+                if not check:
+                    memory.append(c1[i])
+                    memory.append(c1[(i+1) % n])
+                    c1[i], c1[(i+1) % n] = -1, -1
+
+            for i in range(1, n):
+                if c1[(i-1) % n] == c1[(i+1) % n] == -1 and c1[i] != -1:
+                    memory.append(c1[i])
+                    c1[i] = -1
+
+                if c1[i] != -1 and np.random.rand() < threshold:
+                    memory.append(c1[i])
+                    c1[i] = -1
+
+        return self.ls.regret_heuristics([[i for i in cycles1[0] if i != -1], [i for i in cycles1[1] if i != -1]], memory)
+
+    def search(self, population_size=20, limit=100, score_threshold=50, iterations_threshold=300, local=True):
         ss = SteepestSearch(self.ls)
-        populations = np.array([(i, self.ls.get_scores(i)) for i in [ss.search(self.ls.get_random_cycles()) for _ in tqdm(range(population_size))]])
+        populations = np.array([(i, self.ls.get_scores(i)) for i in [ss.search(
+            self.ls.get_random_cycles()) for _ in tqdm(range(population_size))]])
 
         start = time.time()
-        best_scores = []
-        worst_scores = []
+        i = 0
+        best_idx = np.argmin(populations[:, 1])
+        best_score = populations[best_idx][1]
 
-        i, update_i, best_idx = 0, 0, 0
-        current_best = populations[0][1]
+        print("INITIAL SCORE: ", best_score)
 
         while limit > time.time() - start:
+            print((time.time() - start)/limit)
             i += 1
-            indexes = np.arange(population_size)
-            np.random.shuffle(indexes)
+            indexes = np.random.choice(np.arange(population_size), 2)
 
-            worst_i = np.argmax(populations[:,1])
+            worst_i = np.argmax(populations[:, 1])
             worst_score = populations[worst_i][1]
+            result = self.combine_parents(
+                populations[indexes[0]][0], populations[indexes[1]][0])
 
-            result = self.combine(populations[indexes[0]][0], populations[indexes[1]][0])
+            if local:
+                result = ss.search(result)
+            result_score = self.ls.get_scores(np.asarray(result))
 
-            print(np.array(result))
+            if result_score < best_score:
+                populations[best_idx] = result, result_score
+            elif result_score < worst_score and not any(abs(result_score - j) < score_threshold for j in populations[:, 1]):
+                populations[worst_i] = result, result_score
 
-            # if local:
-            #     sol = ss.search(sol, inplace=True)
-            # result_score = self.ls.get_scores(np.array([result[0], result[1]]))
+            best_result, current_best_score = populations[np.argmin(
+                populations[:, 1])]
 
-            # if result_score < current_best:
-            #     populations[best_idx] = result, result_score
-            # elif result_score < worst_score and not any(abs(result_score - j) < score_threshold for j in populations[:,1]):
-            #     populations[worst_i] = result, result_score
+            if current_best_score < best_score:
+                best_score = current_best_score
 
-            # best_result, best_score = populations[np.argmin(populations[:,1])]
-            # best_scores.append(best_score)
-            # worst_scores.append(worst_score)
-
-            # if best_score < current_best:
-            #     current_best = best_score
-            #     update_i = i
-
-            # if i - update_i > iterations_threshold:
-            #     break
-
-        #     if plot:
-        #         clear_output(wait=True)
-        #         plt.figure()
-        #         plot_solution(coords, best_sol)
-        #         plt.title(f'iteration {i} (best: {best_score}, worst: {worst_score})')
-        #         plt.show()
-        #         plt.figure()
-        #         plt.plot(best_scores, label='best score')
-        #         plt.plot(worst_scores, label='worst score')
-        #         plt.legend()
-        #         plt.show()
-
-        return best_result
+        return best_result, best_score
 
 
 if __name__ == '__main__':
     paths = ["C:/Users/Maksim/Desktop/repos/PUT-WI-SI/IMO/LAB5/kroA200.txt",
              "C:/Users/Maksim/Desktop/repos/PUT-WI-SI/IMO/LAB5/kroB200.txt"]
 
-    ls = LocalSearch()
-    ls.read_data(paths[0])
+    times = [1226, 1336]
 
-    es = EvolutionSearch(ls)
-    es.search()
+    for i, path in enumerate(paths):
+        ls = LocalSearch()
+        ls.read_data(path)
 
-    # for path in paths:
-    #     ls = LocalSearch()
-    #     ls.read_data(path)
-    #     ss = SteepestSearch(ls)
+        es = EvolutionSearch(ls)
+        cycles, score = es.search(limit=times[i])
 
-    #     msls_scores = []
-    #     ils1_scores = []
-    #     ils2_scores = []
+        print("FINAL SCORE: ", score)
+        print("============================")
 
-    #     msls_times = []
-
-    #     msls_cycles = []
-    #     ils1_cycles = []
-    #     ils2_cycles = []
-
-    #     for i, _ in enumerate(range(10)):
-    #         start = time.time()
-    #         msls = MSLS(ls, ss)
-    #         cycle = msls.search()
-    #         msls_scores.append(ls.get_scores(cycle))
-    #         msls_cycles.append(cycle)
-    #         msls_times.append(time.time()-start)
-    #         print("MSLS: {0}".format(i))
-
-    #     print("DONE")
-
-    #     for i, _ in enumerate(range(10)):
-    #         ils1 = ILS1(ls)
-    #         ils = ILS(ls, ils1, ss)
-    #         cycle = ils.search(np.mean(msls_times))
-    #         ils1_scores.append(ls.get_scores(cycle))
-    #         ils1_cycles.append(cycle)
-    #         print("ILS1: {0}".format(i))
-
-    #     print("DONE")
-
-    #     for i, _ in enumerate(range(10)):
-    #         ils2 = ILS2(ls)
-    #         ils = ILS(ls, ils2, ss)
-    #         cycle = ils.search(np.mean(msls_times))
-    #         ils2_scores.append(ls.get_scores(cycle))
-    #         ils2_cycles.append(cycle)
-    #         print("ILS2: {0}".format(i))
-
-    #     print("DONE")
-
-    #     print("TIMES")
-    #     print("MIN | MEAN | MAX")
-    #     print(np.min(msls_times), np.mean(msls_times), np.max(msls_times))
-    #     print("SCORES")
-    #     print("MIN | MEAN | MAX")
-    #     print("MSLS")
-    #     print(np.min(msls_scores), np.mean(msls_scores), np.max(msls_scores))
-    #     print("ILS1")
-    #     print(np.min(ils1_scores), np.mean(ils1_scores), np.max(ils1_scores))
-    #     print("ILS2")
-    #     print(np.min(ils2_scores), np.mean(ils2_scores), np.max(ils2_scores))
-
-    #     plt.subplots()
-    #     ls.make_visualizations(
-    #         msls_cycles[np.argmin(msls_scores)][0], color='blue')
-    #     ls.make_visualizations(
-    #         msls_cycles[np.argmin(msls_scores)][1], color='yellow')
-    #     plt.scatter(ls.coordinate_matrix[:, 1],
-    #                 ls.coordinate_matrix[:, 2], color='black')
-    #     plt.show()
-
-    #     plt.subplots()
-    #     ls.make_visualizations(
-    #         ils1_cycles[np.argmin(ils1_scores)][0], color='blue')
-    #     ls.make_visualizations(
-    #         ils1_cycles[np.argmin(ils1_scores)][1], color='yellow')
-    #     plt.scatter(ls.coordinate_matrix[:, 1],
-    #                 ls.coordinate_matrix[:, 2], color='black')
-    #     plt.show()
-
-    #     plt.subplots()
-    #     ls.make_visualizations(
-    #         ils2_cycles[np.argmin(ils2_scores)][0], color='blue')
-    #     ls.make_visualizations(
-    #         ils2_cycles[np.argmin(ils2_scores)][1], color='yellow')
-    #     plt.scatter(ls.coordinate_matrix[:, 1],
-    #                 ls.coordinate_matrix[:, 2], color='black')
-    #     plt.show()
+        plt.subplots()
+        ls.make_visualizations(
+            cycles[0], color='blue')
+        ls.make_visualizations(
+            cycles[1], color='yellow')
+        plt.scatter(ls.coordinate_matrix[:, 1],
+                    ls.coordinate_matrix[:, 2], color='black')
+        plt.show()
